67edd66493b7551407fff931ae3fc6b9e1580544
Nils <40796807+uchars@users.noreply.github.com>
2023-03-23 11:19:32 +0100

    Update main.dart
    forgot to remove scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,63 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'home/home_page.dart';
-import 'settings/locale_provider.dart';
-import 'settings/theme.dart';
-import 'settings/theme_provider.dart';
+import 'auth/auth.dart';
+import 'firebase_options.dart';
+import 'prefs/locale_provider.dart';
+import 'prefs/prefs.dart';
+import 'prefs/theme_provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
-    MultiProvider(
+    MultiBlocProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        ChangeNotifierProvider(create: (_) => LocaleProvider()),
+        BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
       ],
-      child: const EntryPoint(),
+      child: MultiProvider(
+        providers: [
+          Provider<PrefsProvider>(create: (context) => PrefsProvider()),
+          ChangeNotifierProvider<ThemeProvider>(
+            create: (_) => ThemeProvider(),
+          ),
+          ChangeNotifierProvider<LocaleProvider>(
+            create: (_) => LocaleProvider(),
+          ),
+        ],
+        child: const MyApp(),
+      ),
     ),
   );
-  // TODO start IPS process here.
 }
 
-class EntryPoint extends StatelessWidget {
-  const EntryPoint({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var themeProvider = Provider.of<ThemeProvider>(context);
+    var prefsProvider = Provider.of<PrefsProvider>(context);
     var localeProvider = Provider.of<LocaleProvider>(context);
+    var themeProvider = Provider.of<ThemeProvider>(context);
 
     return FutureBuilder(
       future: Future.wait([
-        themeProvider.readTheme(),
+        prefsProvider.loadPrefs(),
         localeProvider.readLocale(),
-        localeProvider.readUnit(),
+        themeProvider.readTheme(),
       ]),
       builder: (context, _) {
         return MaterialApp(
-          title: "GeoStories",
-          home: const HomePage(),
+          title: 'GeoStories',
           themeMode: themeProvider.theme,
-          theme: lightTheme,
-          darkTheme: darkTheme,
+          darkTheme: ThemeData(brightness: Brightness.dark),
+          theme: ThemeData(brightness: Brightness.light),
           locale: localeProvider.locale,
           localizationsDelegates: const [
             AppLocalizations.delegate,
@@ -51,11 +66,35 @@ class EntryPoint extends StatelessWidget {
             GlobalCupertinoLocalizations.delegate,
           ],
           supportedLocales: const [
-            Locale("de"),
             Locale("en"),
+            Locale("de"),
           ],
+          home: const SplashScreen(),
         );
       },
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Splash",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
     );
   }
 }
