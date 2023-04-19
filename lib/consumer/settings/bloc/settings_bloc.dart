170d88840588157678b075ff0914193e98c49b4a
Nils <40796807+uchars@users.noreply.github.com>
2023-04-19 09:55:40 +0200

    Create settings_bloc.dart
    business logic for consumer settings

---
+++ lib/consumer/settings/bloc/settings_bloc.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+abstract class SettingsEvent {}
+
+class SettingsLanguageChangeSelected extends SettingsEvent {}
+
+class SettingsLanguageChanged extends SettingsEvent {}
+
+class SettingsPrivacyOptInSelected extends SettingsEvent {}
+
+class SettingsPrivacyOptInFinished extends SettingsEvent {}
+
+abstract class SettingsState {}
+
+class SettingsInitial extends SettingsState {}
+
+class SettingsShowLanguageChange extends SettingsState {}
+
+class SettingsHideLanguageChange extends SettingsState {}
+
+class SettingsShowPrivacyOptIn extends SettingsState {}
+
+class SettingsHidePrivacyOptIn extends SettingsState {}
+
+class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
+  SettingsBloc() : super(SettingsInitial()) {
+    on<SettingsLanguageChangeSelected>(_onLanguageChangeSelected);
+    on<SettingsLanguageChanged>(_onLanguageChanged);
+    on<SettingsPrivacyOptInSelected>(_onPrivacyOptInSelected);
+    on<SettingsPrivacyOptInFinished>(_onPrivacyOptInFinished);
+  }
+
+  FutureOr<void> _onLanguageChangeSelected(
+    SettingsLanguageChangeSelected event,
+    Emitter<SettingsState> emit,
+  ) {
+    emit(SettingsShowLanguageChange());
+  }
+
+  FutureOr<void> _onLanguageChanged(
+    SettingsLanguageChanged event,
+    Emitter<SettingsState> emit,
+  ) {
+    emit(SettingsHideLanguageChange());
+  }
+
+  FutureOr<void> _onPrivacyOptInFinished(
+    SettingsPrivacyOptInFinished event,
+    Emitter<SettingsState> emit,
+  ) {
+    emit(SettingsHideLanguageChange());
+  }
+
+  FutureOr<void> _onPrivacyOptInSelected(
+    SettingsPrivacyOptInSelected event,
+    Emitter<SettingsState> emit,
+  ) {
+    emit(SettingsShowPrivacyOptIn());
+  }
+}
