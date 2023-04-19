8f01a0b5bba1222e0bd6b2e76e5d0d0e1ac7ce15
Nils <40796807+uchars@users.noreply.github.com>
2023-04-19 09:56:41 +0200

    Create settings_help_group.dart
    settings group for help/about information

---
+++ lib/consumer/settings/groups/settings_help_group.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../widgets/page_transitions.dart';
+import '../../onboarding/onboarding_screen.dart';
+import '../widgets/settings_element.dart';
+import '../widgets/settings_group.dart';
+
+class SettingsHelpGroup extends StatelessWidget {
+  const SettingsHelpGroup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var l10n = AppLocalizations.of(context);
+
+    return SettingsGroup(
+      icon: const FaIcon(FontAwesomeIcons.questionCircle),
+      name: l10n!.settings_help_groupTitle,
+      children: [
+        SettingsElement(
+          l10n.settings_help_tour,
+          onTap: () => Navigator.of(context).push(slideLeftTransition(
+            const OnboardingScreen(),
+          )),
+        ),
+        SettingsElement(l10n.settings_help_about),
+      ],
+    );
+  }
+}
