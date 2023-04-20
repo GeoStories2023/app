136e0766ea894a97e7f2975a21f17f9706ea870d
Nils <40796807+uchars@users.noreply.github.com>
2023-04-20 10:39:47 +0200

    added splashscreen
    for checking things like if the user is already logged in

---
+++ lib/splash/splash_bloc.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../auth/auth.dart';
+import '../prefs/prefs.dart';
+
+abstract class SplashState {}
+
+class SplashInitial extends SplashState {}
+
+class SplashLoginInProgress extends SplashState {}
+
+class SplashLoginFailiure extends SplashState {}
+
+class SplashLoginSuccess extends SplashState {}
+
+class SplashAutologinFalse extends SplashState {}
+
+abstract class SplashEvent {}
+
+class SplashStart extends SplashEvent {}
+
+class _Login extends SplashEvent {}
+
+/// Start connecting to the server
+class _ConnectToServer extends SplashEvent {}
+
+class SplashBloc extends Bloc<SplashEvent, SplashState> {
+  final AuthBloc authBloc;
+
+  SplashBloc(super.initialState, this.authBloc) {
+    on<SplashStart>(_onStart);
+    on<_ConnectToServer>(_connectToServer);
+    on<_Login>(_onLogin);
+  }
+
+  FutureOr<void> _connectToServer(
+    _ConnectToServer event,
+    Emitter<SplashState> emit,
+  ) {}
+
+  FutureOr<void> _onLogin(
+    _Login event,
+    Emitter<SplashState> emit,
+  ) async {
+    emit(SplashLoginInProgress());
+    authBloc.add(AuthAutoLogin());
+    await for (var state in authBloc.stream) {
+      if (state is AuthLoginSuccess) {
+        emit(SplashLoginSuccess());
+      } else if (state is AuthLoginFailure) {
+        emit(SplashLoginFailiure());
+      }
+    }
+  }
+
+  FutureOr<void> _onStart(
+    SplashStart event,
+    Emitter<SplashState> emit,
+  ) async {
+    final method =
+        (await PrefsProvider().preferences).getString("signInMethod");
+    if (method == "" || method == null) {
+      emit(SplashAutologinFalse());
+    } else {
+      add(_Login());
+    }
+  }
+}
---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:geostories/splash/splash_bloc.dart';
+
+import '../auth/auth.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    BlocProvider.of<SplashBloc>(context).add(SplashStart());
+    return Scaffold(
+      body: BlocBuilder<SplashBloc, SplashState>(builder: (context, state) {
+        if (state is SplashLoginSuccess) {
+          //TODO: return homepage
+        } else if (state is SplashAutologinFalse) {
+          return const AuthPage();
+        }
+        return _buildSplashScreen(context);
+      }),
+    );
+  }
+
+  Widget _buildSplashScreen(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/logo_crop.png"),
+          const CircularProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
