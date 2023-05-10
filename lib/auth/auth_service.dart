f24f2c386908cd912df9d1b634ac0e64244e90df
Nils <40796807+uchars@users.noreply.github.com>
2023-05-10 11:11:59 +0200

    doc: further code documentation

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -6,6 +6,7 @@ class AuthService {
 
   User? get currentUser => _firebaseAuth.currentUser;
 
+  /// Sign in with a [email] and [password] combination.
   signInWithEmail(String email, String password) async {
     return await _firebaseAuth.signInWithEmailAndPassword(
       email: email,
@@ -13,11 +14,14 @@ class AuthService {
     );
   }
 
+  /// Change the current users password to [password].
+  /// NOTE: only works applies for users that log in using a email & password combination.
   void changeEmailPassword(String password) async {
     if (currentUser == null) return;
     await currentUser!.updatePassword(password);
   }
 
+  /// Sign up with a [email] and [password] combination.
   signUpWithEmail(String email, String password) async {
     return await _firebaseAuth.createUserWithEmailAndPassword(
       email: email,
@@ -25,6 +29,7 @@ class AuthService {
     );
   }
 
+  /// Sign in using google OAuth
   signInWithGoogle() async {
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
--- lib/repositories/i_consumer_repo.dart
+++ lib/repositories/i_consumer_repo.dart
@@ -4,15 +4,24 @@ import 'package:geostories/bloc/models/consumer_statistics.dart';
 import 'package:geostories/bloc/models/story.dart';
 
 abstract class IConsumerRepo {
+  /// Get [ConsumerStatistics] for the current user.
+  /// NOTE: Implementation will be responsible for getting the current user.
   Future<ConsumerStatistics> getConsumerStatistics();
 
+  /// Get a list of [Achievement] for the current user.
+  /// NOTE: Implementation will be responsible for getting the current user.
   Future<List<Achievement>> getAchievements();
 
+  /// Get the name for the current user.
+  /// NOTE: Implementation will be responsible for getting the current user.
   Future<String> getName();
 
+  /// Set the current users name to [name].
   Future changeName(String name);
 
+  /// Get a list of [ConsumerFriend] for the current user.
   Future<List<ConsumerFriend>> getFriends();
 
+  /// Get a list of [StartedStory] for the current user.
   Future<List<StartedStory>> getStartedStories();
 }
