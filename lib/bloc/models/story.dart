3502a156db2694f6fdd06c427644d1b7a02af80c
Nils <40796807+uchars@users.noreply.github.com>
2023-05-10 11:02:39 +0200

    feat: Story & StartedStory

--- lib/bloc/models/story.dart
+++ lib/bloc/models/story.dart
@@ -1,9 +1,46 @@
 class Story {
-  final String title;
-  final String content;
+  final String storyName;
+  final String storyPictureUrl;
 
   Story({
-    required this.title,
-    required this.content,
+    required this.storyName,
+    required this.storyPictureUrl,
   });
+
+  factory Story.fromJson(Map<String, dynamic> json) {
+    return Story(
+      storyName: json['storyName'],
+      storyPictureUrl: json['storyPictureUrl'],
+    );
+  }
+}
+
+class StartedStory extends Story {
+  final String chapterName;
+  final double progress;
+
+  StartedStory({
+    required this.chapterName,
+    required this.progress,
+    required super.storyName,
+    required super.storyPictureUrl,
+  });
+
+  factory StartedStory.fromJson(Map<String, dynamic> json) {
+    return StartedStory(
+      chapterName: json['chapterName'],
+      progress: json['progress'],
+      storyName: json['storyName'],
+      storyPictureUrl: json['pictureUrl'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'chapterName': chapterName,
+      'progress': progress,
+      'storyName': storyName,
+      'pictureUrl': storyPictureUrl,
+    };
+  }
 }
