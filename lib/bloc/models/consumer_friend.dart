17bb46cd212fbc657a02e726fdd9377beecdfca4
Nils <40796807+uchars@users.noreply.github.com>
2023-05-31 12:33:43 +0200

    add uid to consumer friend

--- lib/bloc/models/consumer_friend.dart
+++ lib/bloc/models/consumer_friend.dart
@@ -1,14 +1,17 @@
 class ConsumerFriend {
+  final String uid;
   final String name;
   final String profilePictureUrl;
 
   const ConsumerFriend({
+    required this.uid,
     required this.name,
     required this.profilePictureUrl,
   });
 
   factory ConsumerFriend.fromJson(Map<String, dynamic> json) {
     return ConsumerFriend(
+      uid: json['uid'],
       name: json['username'],
       profilePictureUrl: "https://companieslogo.com/img/orig/GOOG-0ed88f7c.png",
     );
@@ -16,6 +19,7 @@ class ConsumerFriend {
 
   Map<String, dynamic> toJson() {
     return {
+      'uid': uid,
       'name': name,
       'pictureUrl': profilePictureUrl,
     };
